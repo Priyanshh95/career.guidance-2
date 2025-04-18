f615af81acfc535f359a2f0dd33b82352ca75670
SiddhiSinghal <singhalsiddhi5@gmail.com>
2025-04-18 07:37:23 +0530

    befor deploy

--- user_code.cpp
+++ user_code.cpp
@@ -1,23 +1,6 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int num, reversed = 0, original, digit;
-    cout << "Enter a number: ";  // Extra prompt added (causes partial mismatch)
-    cin >> num;
-
-    original = num;
-
-    while (num > 0) {
-        digit = num % 10;
-        reversed = reversed * 10 + digit;
-        num /= 10;
-    }
-
-    if (original == reversed)
-        cout << original << " is a Palindrome number." << endl;  // Extra text
-    else
-        cout << original << " is Not a Palindrome number." << endl;
-
-    return 0;
-}
+#include<iostream>
+using namespace std;
+int main() {
+    // Write your code here
+    return 0;
+}
