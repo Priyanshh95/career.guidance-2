7a60f2a943699914c72ada80ebc4c54b9050a0a3
SiddhiSinghal <singhalsiddhi5@gmail.com>
2025-04-27 20:59:33 +0530

    pls

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from app import app
 
 # If you have any app-specific configuration for production, add it here
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
